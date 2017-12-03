502696be6d0cac15f70d5e1d2b6854225f794726
melodytempleton <mtempletoncantu@gmail.com>
2017-12-02 21:05:00 -0600

    First Commit

---
+++ SprintNumberXX.js
@@ -0,0 +1,30 @@
+  var sprintStart = new Date();
+  sprintStart.setMonth(10);
+  sprintStart.setDate(23);
+  sprintStart.setHours(7);
+  sprintStart.setMinutes(0);
+
+  
+  var projectTime = {
+    Thanksgiving: 6,
+    techDesign: 8,
+    codeBuild: 13,
+    componentTesting: 5,
+    codeReview: 4,
+    testing: 5,
+    fixBugs: 4,      
+    fixBugsaaa: 5   
+  }
+  
+    var personalTime = {
+    Thanksgiving: 6,
+    techDesign: 8,
+    codeBuild: 13,
+    componentTesting: 5,
+    codeReview: 4,
+    ReviewChrisCode: 2,
+    ReviewDanCode: 2,
+    fixBugs: 4,      
+    fixBugsaaa: 5   
+  }
+  
